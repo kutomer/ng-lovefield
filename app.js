1e5b14b7735f4bf9a8b18a11ded7d1987a5b5eef
kutomer <kruvitomer1991@gmail.com>
2016-01-19 01:41:24 +0200

    github page changes update README update html base-url use CDN instead of bower enable html5Mode

--- app.js
+++ app.js
@@ -4,10 +4,9 @@ angular.module('ng-lovefield-demo', ['ngRoute', 'kutomer.ng-lovefield']).
 		config(['$routeProvider', '$locationProvider',
 			function($routeProvider, $locationProvider) {
 
-				//can't use this with github demo / if don't have access to the server
-				$locationProvider.html5Mode(false);
+				$locationProvider.html5Mode(true);
 
-				var staticPath = '/';
+				var staticPath = '';
 				var appPathRoute = '/';
 				var pagesPath = staticPath + 'demo/';
 
